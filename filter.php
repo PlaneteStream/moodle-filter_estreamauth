fb77699cdde208b43841975c08531550348e39d4
Planet eStream <support@planetestream.co.uk>
2017-11-15 16:03:32 +0000

    Add files via upload
    Added the latest version of the plugin as of November 15th 2017. This is to bring all eStream plugins on Github up-to-date at once.

--- filter.php
+++ filter.php
@@ -1,4 +1,26 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * Planet eStream Authentication Plugin
+ *
+ * @package    filter_estreamauth
+ * @copyright  Planet eStream
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
+ */
 // require_once('../../config.php');
 defined('MOODLE_INTERNAL') || die();
 class filter_estreamauth extends moodle_text_filter {
@@ -23,10 +45,9 @@ class filter_estreamauth extends moodle_text_filter {
         }
         return urlencode($strobfuscated);
     }
-	
     public function filter($text, array $options = array()) {
-			global $USER;
-            $delta = $this->userobfuscate($USER->username);
+        global $USER;
+        $delta = $this->userobfuscate($USER->username);
         return str_replace('ESDLTA', $delta, $text);
-        }
+    }
 }
--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 $plugin            = new StdClass();
 $plugin->component = 'filter_estreamauth';
-$plugin->version   = 2017030100;
+$plugin->version   = 2017032000;
 $plugin->requires  = 2012062500;
-$plugin->release   = '6.30';
+$plugin->release   = '6.32';
 $plugin->maturity  = MATURITY_STABLE;
